a6193164d2dffc66305cf5ea47d0636bcaf04669
GLISSANDO012-PC\glissando012 <yamane@glissando.co.jp>
2023-08-01 14:43:48 +0900

    20230801_並び替えボタン_登録順／期限順

--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -24,12 +24,14 @@
     <!-- task表示 -->
     <div class="task-wrap">
       <div class="task-box">
-        <pre>{{ $data }}</pre>
+        <!-- <pre>{{ $data }}</pre> -->
         <div class="task-sort">
-          <button v-on:click="sort()">並び替え</button>
+          <button v-on:click="sortArry()">並び替え (登録順)</button>
+          <button v-on:click="sortDate()">並び替え (期限日)</button>
+
         </div>
         <ul>
-          <li v-for="(task, index) in tasks" :key="(index,task.deadline)" v-bind:class=task.closeclass>
+          <li v-for="(task, index) in tasks" :key="(index,task.id)" v-bind:class=task.closeclass>
             <div class="content">
               <h3>{{ task.name}}</h3>
               <p>{{ task.content}}</p>
@@ -52,6 +54,9 @@
 
 <script>
 let task = new Object();
+let sortFlag = "desc";
+let sortDateFlag = "datedesc";
+
 export default {
   name: 'TaskCard',
   data(){
@@ -61,7 +66,7 @@ export default {
       taskDeadline:"",
       num:1,
       tasks:[],
-
+      
     }
 
   },
@@ -72,7 +77,7 @@ export default {
           //console.log('click')
           if( this.newTask === "" ) return //未入力なら実行しない
           if( this.taskContent === "" ) return //未入力なら実行しない
-          task = {
+          task = {//入力内容・状態・class名を格納
             id:this.num++,
             name:this.newTask,
             content:this.taskContent,
@@ -81,39 +86,59 @@ export default {
             stateclass:this.stateclass = "",
             closeclass:this.closeclass = ""
           }
-          this.tasks.push(task),
-          this.newTask = "",
-          this.taskContent = "",
-          this.taskDeadline = ""
+          this.tasks.push(task),//tasksにtaskの内容を入れる
+          this.newTask = "",//入力内容を削除・初期表示へ
+          this.taskContent = "",//入力内容を削除・初期表示へ
+          this.taskDeadline = ""//入力内容を削除・初期表示へ
         },
 
 
         //状態（未着手・処理中・完了）ボタン処理
         change:function(index){
-          if(this.tasks[index].states === "未着手"){
+          if(this.tasks[index].states === "未着手"){//未着手なら
             this.tasks[index].states = "処理中";
             this.tasks[index].stateclass = "processing";
             this.tasks[index].closeclass = ""
           }
-          else if (this.tasks[index].states === "処理中"){
+          else if (this.tasks[index].states === "処理中"){//処理中なら
             this.tasks[index].states = "完了";
             this.tasks[index].stateclass = "";
             this.tasks[index].closeclass = "complete"
           }
-          else if (this.tasks[index].states === "完了"){
+          else if (this.tasks[index].states === "完了"){//完了なら
             this.tasks[index].states = "処理中";
             this.tasks[index].stateclass = "processing";
             this.tasks[index].closeclass = ""
           }
         },
 
-        //期限で並び替え
-        sort:function(){
-          console.log(this.task.deadline)
+        //ID 登録順で並び替え
+        sortArry:function(){
+          //タスクID 降順
+          if(sortFlag === "asc"){
+            sortFlag = "desc";
+            this.tasks.sort((a, b) => { return b.id - a.id});
+          }
+          //タスクID 昇順
+          else if(sortFlag === 'desc') {
+            sortFlag = 'asc';
+            this.tasks.sort((a, b) => { return a.id - b.id});
+          }
+        },
+
+       //期限で並び替え
+        sortDate:function(){
+          //日付降順
+          if(sortDateFlag === "dateasc"){
+            sortDateFlag = "datedesc";
+            this.tasks.sort((a, b) => { return (a.deadline < b.deadline ? 1 : -1); });
+          }
+          //日付昇順
+          else if(sortDateFlag === 'datedesc') {
+            sortDateFlag = 'dateasc';
+            this.tasks.sort((a, b) => { return (a.deadline > b.deadline ? 1 : -1); });
+          }
         }
-
-
-
       }
 }
 </script>
@@ -174,14 +199,16 @@ export default {
 .task-wrap .task-box .task-sort{
   margin: 0 0 20px;
   background: none;
+  display: flex;
+  justify-content: center;
 }
 .task-wrap .task-box .task-sort button{
   border: solid 1px #333;
   background: #fff;
   display: block;
   padding: 5px 0;
-  width: 100px;
-  margin: 0 auto;
+  width: 150px;
+  margin: 0 20px;
 }
 
 .task-wrap .task-box .task-sort button:hover{
